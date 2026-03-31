698c7c1a522c40359ebb72c1345f7397855e405a
Toffee <toastpan.main@gmail.com>
2026-03-31 16:44:50 +0200

    d

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"http://194.117.224.119:3551"
+#define API_URL @"http://85.0.248.87:3551"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
